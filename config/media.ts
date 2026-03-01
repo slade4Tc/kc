972705718c88499cd946761bc437d0a67ff856cc
Slade4T <captainzeroseven@gmail.com>
2026-03-01 23:54:24 +0200

    Update media.ts

--- config/media.ts
+++ config/media.ts
@@ -1,1 +1,2 @@
-export const IMAGE_BASE_URL = 'https://kc-images.kc-test-v1.workers.dev';
+export const IMAGE_BASE_URL =
+  (process.env.NEXT_PUBLIC_IMAGE_BASE_URL ?? 'https://pub-81e8ad22dc1f4213a0c54798e94af7b1.r2.dev').replace(/\/+$/, '');
