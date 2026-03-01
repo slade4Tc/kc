4b9764c5ba48c72dab4cad4b9052256ee7c9b4b5
Slade4T <captainzeroseven@gmail.com>
2026-03-01 23:10:23 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+
+  images: {
+    // IMPORTANT for static export
+    unoptimized: true,
+
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'kc-images.kc-test-v1.workers.dev',
+      },
+      // (optional) αν κρατάς και r2.dev links κάπου
+      {
+        protocol: 'https',
+        hostname: 'pub-81e8ad22dc1f4213a0c54798e94af7b1.r2.dev',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
